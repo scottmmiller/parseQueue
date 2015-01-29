f755d42c7d62256b820feb6322616d94ef6e9577
skootdiggity <smmiller.work@gmail.com>
2015-01-29 12:21:33 -0700

    sending to Parse but not showing question string

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,6 +2,16 @@ var app = angular.module('parseQ');
 
 app.controller('MainController', function($scope, parseService) {
 
-	$scope.test = 'Can you see me NOW?!?!'
+	$scope.test = 'Can you see me NOW?!?!';
+
+	$scope.postData = function() {
+		var question = $scope.questionRequest;
+		parseService.postQuestion(question).then(function(serviceResponse){
+			console.log(serviceResponse)
+		}, function(error) {
+			console.log(error);
+		})
+
+	};
 
 });
--- js/services/parseService.js
+++ js/services/parseService.js
@@ -3,17 +3,20 @@ var app = angular.module('parseQ');
 app.service('parseService', function($http, $q) {
 	
 
-	this.postQuestion = function(question) {
+	this.postQuestion = function(userQuestion) {
 		var deferred = $q.defer();
 
 		$http({
 			method: 'POST',
 			url: 'https://api.parse.com/1/classes/questions',
 			data: {
-				question: question
+				question: userQuestion
 			}
-		})
-		deferred.resolve(question);
+		}).then(function(httpResponse) {
+			deferred.resolve(httpResponse);
+		}, function(error) {
+			deferred.reject(error);
+		});
 		return deferred.promise;
 	};
 });
